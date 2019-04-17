d9d2c1c4d01e75c3f97a16e68821ebf6f177acb5
Drez0 <alexandr.fdh@gmail.com>
2019-04-17 11:25:44 +0300

    Added test script

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const { promisify } = require('util');
 const { Server } = require('./bin');
 
-const serverInstance = promisify(Server.listen.bind(Server));
+const { NODE_ENV, SERVER_PORT } = process.env;
 
-const { SERVER_PORT } = process.env;
+const serverInstance = promisify(Server.listen.bind(Server));
 
 (async () => {
   try {
     await serverInstance(SERVER_PORT);
     // eslint-disable-next-line no-console
-    console.log('Server Running!');
+    console.log(
+      `🚀 Server ready at https://localhost:${SERVER_PORT} (mode: ${NODE_ENV})`,
+    );
   } catch (err) {
     // eslint-disable-next-line no-console
     console.log('Error happened during server start', err);
