256bef290d34aef5368e5b5f2336a90343e2996b
Drez0 <alexandr.fdh@gmail.com>
2019-04-17 11:48:39 +0300

    Set up proxy setting for app instance

--- src/bin/app.js
+++ src/bin/app.js
@@ -3,6 +3,14 @@ const helmet = require('koa-helmet');
 
 const app = new Koa();
 
+const { NODE_ENV } = process.env;
+
+const isProduction = NODE_ENV === 'production';
+
+if (isProduction) {
+  app.proxy = true; // eg header 'X-Forwarded-Host'
+}
+
 app.use(helmet());
 
 module.exports = app;
